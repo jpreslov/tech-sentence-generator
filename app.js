d2ac16ee0194e8489db7a62d88c43ce2910ec735
jpreslov <lovett_jp@outlook.com>
2022-02-01 16:57:23 -0600

    color

--- app.js
+++ app.js
@@ -1,15 +1,15 @@
 const adjectives = ['exponential', 'innovative', 'multicloud', 'cloud-based', 'scalable', 'company-wide', 'automated', 'encrypted']
 const singularNouns = ['innovation', 'technology', 'synergy', 'solution', '']
-const pluralNouns = ['innovations', 'technologies', 'synergies', 'solutions', 'facets', 'implementations', 'people', 'societies', 'nations']
+const pluralNouns = ['innovations', 'technologies', 'synergies', 'solutions', 'facets', 'implementations', 'people', 'societies', 'nations', 'systems']
 const adverbs = ['elegantly', 'constantly', 'intelligently', 'relentlessly', 'always', 'never', 'indubitably', 'flawlessly']
-const pastTenseVerbs = ['implemented', 'constructed', 'concocted', 'developed', 'strategized']
-// const pastTenseVerbs = ['implemented', 'constructed', 'concocted', 'developed', 'strategized']
+const pastVerbs = ['implemented', 'constructed', 'concocted', 'developed', 'strategized']
+const presentVerbs = ['implement', 'construct', 'concoct', 'develop', 'strategize', 'grow', 'help', 'continue']
+const helpingVerbs = ['am', 'is', 'are', 'was', 'were', 'being', 'been', 'will be', 'has', 'have', 'had', 'having', 'will have', 'does', 'do', 'did', 'will do']
 const pronouns = ['they', 'we']
 const possessivePronouns = ['their', 'our']
-const helpingVerbs = ['am', 'is', 'are', 'was', 'were', 'being', 'been', 'will be', 'has', 'have', 'had', 'having', 'will have', 'does', 'do', 'did', 'will do']
-const nouns = ['damage', 'harm', 'positive', 'good']
-const linkingVerbs = ['for', 'to', 'with']
-const determiners = ['many', 'few', 'countless']
+const nouns = ['damage', 'harm', 'positive', 'good', 'evil']
+const linkingVerbs = ['for', 'to', 'with', 'in', 'around', 'regarding', 'alongside']
+const determiners = ['many', 'few', 'countless', 'no']
 const container = document.getElementById('container')
 const btn = document.getElementById('generate-btn')
 const sentenceDisplayDiv = document.createElement('div')
@@ -20,9 +20,14 @@ let randomNum = (arr) => {
 }
 
 const makeSentence = () => {
-  const sentence = `${possessivePronouns[randomNum(possessivePronouns)]} ${adjectives[randomNum(adjectives)]} 
+  const words = `${possessivePronouns[randomNum(possessivePronouns)]} ${adjectives[randomNum(adjectives)]} 
   ${pluralNouns[randomNum(pluralNouns)]} ${helpingVerbs[randomNum(helpingVerbs)]} ${adjectives[randomNum(adjectives)]} ${nouns[randomNum(nouns)]} 
-  ${linkingVerbs[randomNum(linkingVerbs)]} ${determiners[randomNum(determiners)]} ${pluralNouns[randomNum(pluralNouns)]}.`
+  ${linkingVerbs[randomNum(linkingVerbs)]} ${determiners[randomNum(determiners)]} ${pluralNouns[randomNum(pluralNouns)]} ${linkingVerbs[randomNum(linkingVerbs)]}
+  ${adjectives[randomNum(adjectives)]}, ${adjectives[randomNum(adjectives)]} ${pluralNouns[randomNum(pluralNouns)]}.`
+
+  const sentence = `${words.charAt(0).toUpperCase() + words.slice(1)}`
+  const saveBtn = document.createElement('button')
+  // saveBtn.addEventListener('click', myFunction)
   blockQuote.innerHTML = sentence
   blockQuote.setAttribute('id', 'quote')
   sentenceDisplayDiv.append(blockQuote)
