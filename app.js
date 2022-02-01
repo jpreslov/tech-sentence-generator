e38c0c9c1effc3a80a2bd131920627e07c3f0d6a
jpreslov <lovett_jp@outlook.com>
2022-02-01 15:54:55 -0600

    woo

--- app.js
+++ app.js
@@ -9,6 +9,8 @@ const pronouns = ['they', 'we']
 const possessivePronouns = ['their', 'our']
 const helpingVerbs = ['am', 'is', 'are', 'was', 'were', 'being', 'been', 'will be', 'has', 'have', 'had', 'having', 'will have', 'does', 'do', 'did', 'will do']
 const nouns = ['damage', 'harm', 'positive', 'good']
+const linkingVerbs = ['for', 'to', 'with']
+const determiners = ['many', 'few', 'countless']
 const container = document.getElementById('container')
 const btn = document.getElementById('generate-btn')
 const sentenceDisplayDiv = document.createElement('div')
@@ -20,8 +22,10 @@ let randomNum = (arr) => {
 
 const makeSentence = () => {
   const sentence = `${possessivePronouns[randomNum(possessivePronouns)]} ${adjectives[randomNum(adjectives)]} 
-  ${pluralNouns[randomNum(pluralNouns)]} ${helpingVerbs[randomNum(helpingVerbs)]} ${adjectives[randomNum(adjectives)]} ${nouns[randomNum(nouns)]} `
+  ${pluralNouns[randomNum(pluralNouns)]} ${helpingVerbs[randomNum(helpingVerbs)]} ${adjectives[randomNum(adjectives)]} ${nouns[randomNum(nouns)]} 
+  ${linkingVerbs[randomNum(linkingVerbs)]} ${determiners[randomNum(determiners)]}.`
   blockQuote.innerHTML = sentence
+  blockQuote.id = 'quote'
   sentenceDisplayDiv.append(blockQuote)
   container.append(sentenceDisplayDiv)
 }
