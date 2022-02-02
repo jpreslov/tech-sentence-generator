8d758bbd5dd45722b90677df42c58050f893bf9b
jpreslov <lovett_jp@outlook.com>
2022-02-02 13:11:33 -0600

    adding objects

--- app.js
+++ app.js
@@ -1,33 +1,56 @@
 const adjectives = ['exponential', 'innovative', 'multicloud', 'cloud-based', 'scalable', 'company-wide', 'automated', 'encrypted']
-const singularNouns = ['innovation', 'technology', 'synergy', 'solution', '']
-const pluralNouns = ['innovations', 'technologies', 'synergies', 'solutions', 'facets', 'implementations', 'people', 'societies', 'nations', 'systems']
 const adverbs = ['elegantly', 'constantly', 'intelligently', 'relentlessly', 'always', 'never', 'indubitably', 'flawlessly']
-const pastVerbs = ['implemented', 'constructed', 'concocted', 'developed', 'strategized']
-const presentVerbs = ['implement', 'construct', 'concoct', 'develop', 'strategize', 'grow', 'help', 'continue']
-const helpingVerbs = ['am', 'is', 'are', 'was', 'were', 'being', 'been', 'will be', 'has', 'have', 'had', 'having', 'will have', 'does', 'do', 'did', 'will do']
-const pronouns = ['they', 'we']
-const possessivePronouns = ['their', 'our']
 const nouns = ['damage', 'harm', 'positive', 'good', 'evil']
-const linkingVerbs = ['for', 'to', 'with', 'in', 'around', 'regarding', 'alongside']
 const determiners = ['many', 'few', 'countless', 'no']
+
 const container = document.getElementById('container')
 const btn = document.getElementById('generate-btn')
 const sentenceDisplayDiv = document.createElement('div')
 const blockQuote = document.createElement('blockquote')
 
-let randomNum = (arr) => {
+const singPlur = () => {
+  let arr = ['sing', 'plur']
+  return (Math.floor(Math.random() * 2))
+}
+
+const verbs = {
+  past: {
+    helping: ['have done', 'did', 'was', 'were', 'had', 'has'],
+    reg: ['implemented', 'constructed', 'concocted', 'developed', 'strategized'],
+},
+  present: {
+    reg: ['implement', 'construct', 'concoct', 'develop', 'strategize', 'grow', 'help', 'continue'],
+    helping: ['is', 'are', 'being', 'having', 'does', 'do']
+  },
+  future: {
+    helping: ['will be', 'will have', 'will do']
+  },
+}
+const prepositions = ['for', 'to', 'with', 'in', 'around', 'regarding', 'alongside']
+
+const nouns = {
+  singularNouns: ['innovation', 'technology', 'synergy', 'solution'],
+  pluralNouns: ['innovations', 'technologies', 'synergies', 'solutions', 'facets', 'implementations', 'people', 'societies', 'nations', 'systems']
+}
+
+const pronouns = {
+  pronouns: ['they', 'we'],
+  possessivePronouns: ['their', 'our']
+
+}
+
+const randomNum = (arr) => {
   return Math.floor(Math.random() * arr.length)
 }
 
 const makeSentence = () => {
   const words = `${possessivePronouns[randomNum(possessivePronouns)]} ${adjectives[randomNum(adjectives)]} 
-  ${pluralNouns[randomNum(pluralNouns)]} ${helpingVerbs[randomNum(helpingVerbs)]} ${adjectives[randomNum(adjectives)]} ${nouns[randomNum(nouns)]} 
-  ${linkingVerbs[randomNum(linkingVerbs)]} ${determiners[randomNum(determiners)]} ${pluralNouns[randomNum(pluralNouns)]} ${linkingVerbs[randomNum(linkingVerbs)]}
+  ${pluralNouns[randomNum(pluralNouns)]} ${verbs.helpingVerbs[randomNum(verbs.helpingVerbs)]} ${adjectives[randomNum(adjectives)]} ${nouns[randomNum(nouns)]} 
+  ${verbs.linkingVerbs[randomNum(verbs.linkingVerbs)]} ${determiners[randomNum(determiners)]} ${pluralNouns[randomNum(pluralNouns)]} ${verbs.linkingVerbs[randomNum(verbs.linkingVerbs)]}
   ${adjectives[randomNum(adjectives)]}, ${adjectives[randomNum(adjectives)]} ${pluralNouns[randomNum(pluralNouns)]}.`
 
   const sentence = `${words.charAt(0).toUpperCase() + words.slice(1)}`
   const saveBtn = document.createElement('button')
-  // saveBtn.addEventListener('click', myFunction)
   blockQuote.innerHTML = sentence
   blockQuote.setAttribute('id', 'quote')
   sentenceDisplayDiv.append(blockQuote)
